545416acb3862862def510f56aa54c4b54e63723
choonshon <30280536+choonshon@users.noreply.github.com>
2021-06-30 17:58:36 +0900

    페북 로그인 및 계정 연동 기능 구현 (#3)
    * Facebook Button 연동 중
    
    * swift ui 적용중
    
    * facebook 인증까지
    
    * service 연동 테스트 중
    
    * 구글 계정으로 로그인한 계정에 페북 연동

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,15 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var user: User?
+    
+    init() {
+        
+    }
     
     var body: some View {
-        NavigationView {
-            SignInView()
+        VStack {
+            if let user = user {
+                UserView(user: user)
+            } else {
+                SignInView()
+            }
         }
+        .onReceive(NotificationCenter.default.publisher(for: .signInProcessCompleted), perform: { notification in
+            self.user = notification.object as? User
+        })
+        .onReceive(NotificationCenter.default.publisher(for: .signOutProcessCompleted), perform: { notification in
+            self.user = nil
+        })
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
--- Shared/SignInView.swift
+++ Shared/SignInView.swift
@@ -1,123 +1,70 @@
 //
-//  SignIn.swift
+//  SignInView2.swift
 //  VideoChat
 //
-//  Created by Choon Shon on 2021/06/14.
+//  Created by Choon Shon on 2021/06/24.
 //
 
 import SwiftUI
-import UIKit
-import GoogleSignIn
-import RxSwift
-import RxCocoa
-import Firebase
-import FirebaseFirestore
-import GoogleSignIn
+import URLImage
 
-struct SignInView: UIViewControllerRepresentable {
-    
-    func makeUIViewController(context: Context) -> SignInViewController {
-        return SignInViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: SignInViewController, context: Context) {
-        
+struct SignInView: View {
+
+    var body: some View {
+        VStack {
+            Image("welcomeCharacter")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.top, 20)
+                .frame(width: 200)
+            
+            Text("Welcome to\nBetter Me!")
+                .fontWeight(.black)
+                .foregroundColor(Color(.systemIndigo))
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            Text("Become the best version of yourself by tracking your every move.")
+                .fontWeight(.light)
+                .multilineTextAlignment(.center)
+                .padding()
+
+            
+            VStack(spacing: 10) {
+                // Google Button
+                Button(action: {
+                    SignIn.shared.getService(.google).signIn()
+                }) {
+                    Text("Sign in With Google")
+                }
+                .buttonStyle(AuthenticationButtonStyle())
+                
+                // Facebook Button
+                Button(action: {
+                    SignIn.shared.getService(.facebook).signIn()
+                }) {
+                    Text("Sign in With Facebook")
+                }
+                .buttonStyle(AuthenticationButtonStyle())
+            }
+            .padding()
+        }
     }
 }
 
-struct SignIn_Previews: PreviewProvider {
+struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
         SignInView()
     }
 }
 
-class SignInViewController: UIViewController {
-    
-    private let googleSiginInButton: UIButton = {
-        let button = UIButton.init(type: .custom)
-        button.setTitleColor(.blue, for: .normal)
-        button.setTitle("Join with Google", for: .normal)
-        return button
-    }()
-    
-    private let changeNicknameButton: UIButton = {
-        let button = UIButton.init(type: .custom)
-        button.setTitleColor(.blue, for: .normal)
-        button.setTitle("Change nickname to star8080", for: .normal)
-        return button
-    }()
-    
-    private let currentUserInfoLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 10.0)
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    private let disposeBag = DisposeBag()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        GIDSignIn.sharedInstance().presentingViewController = self
-        
-        let user = Auth.auth().currentUser
-        //self.currentUserInfoLabel.text = user.debugDescription
-        
-        db.collection("users").document(user?.uid ?? "").getDocument { snapshot, error in
-            self.currentUserInfoLabel.text = snapshot?.get("nickname") as? String
-        }
-       
-        setupViews()
-        bindViews()
-    }
-    
-    private func setupViews() {
-        let vStackView = UIStackView()
-        vStackView.axis = .vertical
-        
-        view.addSubview(vStackView)
-        vStackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            vStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20.0),
-            vStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20.0),
-            vStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-        
-        vStackView.addArrangedSubview(googleSiginInButton)
-        vStackView.addArrangedSubview(changeNicknameButton)
-        vStackView.addArrangedSubview(currentUserInfoLabel)
-    }
-    
-    private func bindViews() {
-        googleSiginInButton.rx
-            .tap
-            .subscribe(onNext: {
-                GIDSignIn.sharedInstance().signIn()
-            })
-            .disposed(by: disposeBag)
-        
-        changeNicknameButton.rx
-            .tap
-            .subscribe(onNext: { [weak self] in
-                self?.changeNickname()
-            })
-            .disposed(by: disposeBag)
-        
-        NotificationCenter.default.rx.notification(.init(Login.EventName.signInProcessCompleted))
-            .debug()
-            .compactMap { $0.object.debugDescription }
-            .bind(to: currentUserInfoLabel.rx.text)
-            .disposed(by: disposeBag)
-    }
-    
-    func changeNickname() {
-        let currentUser = Auth.auth().currentUser
-        guard let uid = currentUser?.uid else { return }
-        
-        db.collection("users").document(uid).updateData(["nickname": "star8080"]) { error in
-    
-        }
+struct AuthenticationButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(configuration.isPressed ? Color(.systemIndigo).opacity(0.7) : Color(.systemIndigo))
+            .cornerRadius(12)
     }
 }
-
